c62fcd1ecb68e5f2a09437b121d6f83d508928af
Fredrik Schmidt <froddanz@outlook.com>
2017-09-15 15:11:12 +0200

    Retargeted FrodLib.Core to .NET Standard 1.1

--- FrodLib.Core/Properties/AssemblyInfo.cs
+++ FrodLib.Core/Properties/AssemblyInfo.cs
@@ -6,12 +6,6 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FrodLib.Core")]
-[assembly: AssemblyDescription("A class library that contains useful utilities and extensions for multiple platform (portable library)")]
-[assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyCompany("Fredrik Schmidt")]
-[assembly: AssemblyProduct("FrodLib")]
-[assembly: AssemblyCopyright("Copyright © Fredrik Schmidt 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -26,8 +20,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.7.66")]
-[assembly: AssemblyInformationalVersion("1.3.7.66")]
+
 //[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: InternalsVisibleTo("FrodLib.Tests")]
 [assembly: InternalsVisibleTo("FrodControls")]
